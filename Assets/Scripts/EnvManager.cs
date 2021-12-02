7482db34aea8dae62c91ad308e56e1f9422cad8a
Yuji Sato <yujisatojr@gmail.com>
2021-12-02 00:41:27 -1000

    Add button control and env manager

---
+++ Assets/Scripts/EnvManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EnvManager : MonoBehaviour
+{
+    public static EnvManager Instance;
+    private int maxHealth = 300;
+    public int health;
+    public int score;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+ 
+
+    private void Start()
+    {
+        health = maxHealth;
+    }
+
+    private void Update()
+    {
+
+    }
+
+    public void setHealth(int damage)
+    {
+        health += damage;
+    }
+
+    public int getHealth()
+    {
+        return health;
+    }
+
+    public int getScore()
+    {
+        return score;
+    }
+}
