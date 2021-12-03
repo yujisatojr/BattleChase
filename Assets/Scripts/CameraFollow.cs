242f42c776cf379e86040d408e7c9ebff6b491c8
Yuji Sato <yujisatojr@gmail.com>
2021-12-02 17:38:55 -1000

    Allow cameras to have smooth movement

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,50 +4,32 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    [SerializeField]
-     private Transform target;
- 
-     [SerializeField]
-     private Vector3 offsetPosition;
- 
-     [SerializeField]
-     private Space offsetPositionSpace = Space.Self;
- 
-     [SerializeField]
-     private bool lookAt = true;
- 
-     private void LateUpdate()
-     {
-         Refresh();
-     }
- 
-     public void Refresh()
-     {
-         if(target == null)
-         {
-             Debug.LogWarning("Missing target ref !", this);
- 
-             return;
-         }
- 
-         // compute position
-         if(offsetPositionSpace == Space.Self)
-         {
-             transform.position = target.TransformPoint(offsetPosition);
-         }
-         else
-         {
-             transform.position = target.position + offsetPosition;
-         }
- 
-         // compute rotation
-         if(lookAt)
-         {
-             transform.LookAt(target);
-         }
-         else
-         {
-             transform.rotation = target.rotation;
-         }
-     }
+    public Transform target;
+    public Vector3 offset;
+    public float smoothSpeed = 0.125f;
+    public Space offsetPositionSpace = Space.Self;
+    public bool lookAt = true;
+    
+    private void LateUpdate()
+    {
+        if(offsetPositionSpace == Space.Self)
+        {
+            transform.position = target.TransformPoint(offset);
+        }
+        else
+        {
+            Vector3 desiredPosition = target.position + offset;
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+            transform.position = smoothedPosition;
+        }
+
+        if(lookAt)
+        {
+            transform.LookAt(target);
+        }
+        else
+        {
+            transform.rotation = target.rotation;
+        }
+    }
 }
